09fa91359f7157579b974d6cd5d9d97f7e787c56
philopence <epcroo@yeah.net>
2024-02-23 13:58:13 +0800

    fix: component element

--- src/App.jsx
+++ src/App.jsx
@@ -15,15 +15,15 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={Dashboard} />
-          <Route path="bookings" element={Bookings} />
-          <Route path="cabins" element={Cabins} />
-          <Route path="users" element={NewUsers} />
-          <Route path="settings" element={Settings} />
-          <Route path="account" element={Account} />
-          <Route path="login" element={Login} />
-          <Route path="*" element={PageNotFound} />
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<NewUsers />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </>
