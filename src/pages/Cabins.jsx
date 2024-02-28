a639fd56a01ccf709cca6c03a543a6603e619b4a
philopence <epcroo@yeah.net>
2024-02-28 16:25:06 +0800

    build cabins table

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,21 +1,18 @@
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  }, []);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img
-        src="https://njagkilqndcxzvosenkj.supabase.co/storage/v1/object/public/cabins-image/cabin-001.jpg"
-        alt="cabin image"
-      />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>TEST</p>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
